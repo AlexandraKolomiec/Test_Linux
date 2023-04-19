7681153885746a511c0fa355dc849e96b235e144
Alexandra Kolomiets <Sasha-Km@yandex.ru>
2023-04-19 21:42:52 +0300

    correct

--- DZ1.py
+++ DZ1.py
@@ -2,13 +2,12 @@ import subprocess
 import re
 
 if __name__ == '__main__':
-    result = subprocess.run('cat /etc/os-release', shell=True,
-                            stdout=subprocess.PIPE, encoding='utf 8')
-    out = result.stdout
-    #print(out)
-
 
     def func(com: str, text: str):
+        result = subprocess.run('cat /etc/os-release', shell=True,
+                                stdout=subprocess.PIPE, encoding='utf 8')
+        out = result.stdout
+        # print(out)
         if result.returncode == 0:
             if re.search(r'jammy', out):
                 print(True)
