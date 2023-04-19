f594d99632fb61f1c1f1f31f7fea855e2670233a
Alexandra Kolomiets <Sasha-Km@yandex.ru>
2023-04-19 21:43:16 +0300

    correct

--- DZ1-2.py
+++ DZ1-2.py
@@ -4,14 +4,15 @@ import string
 string.punctuation
 
 if __name__ == '__main__':
-    result = subprocess.run('cat /etc/os-release', shell=True,
-                            stdout=subprocess.PIPE, encoding='utf 8')
-    out = result.stdout
-    for p in string.punctuation:
-        if p in out:
-            out1 = out.replace(p, ' ')
 
     def func(com: str, text: str):
+        result = subprocess.run('cat /etc/os-release', shell=True,
+                                stdout=subprocess.PIPE, encoding='utf 8')
+        out = result.stdout
+        for p in string.punctuation:
+            if p in out:
+                out1 = out.replace(p, ' ')
+
         if result.returncode == 0:
             if re.search(r'jammy', out1):
                 print(True)
